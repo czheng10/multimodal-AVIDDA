c28d6914ba6190c86b0a97566b37f9d03898f88d
Cindy Zheng <czheng03@mit.edu>
2025-04-28 20:36:59 -0400

    Initial Commit

---
+++ AVIDDA/AVIDDAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVIDDAApp.swift
+//  AVIDDA
+//
+//  Created by Cindy Zheng on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AVIDDAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVIDDATests/AVIDDATests.swift
@@ -0,0 +1,17 @@
+//
+//  AVIDDATests.swift
+//  AVIDDATests
+//
+//  Created by Cindy Zheng on 4/28/25.
+//
+
+import Testing
+@testable import AVIDDA
+
+struct AVIDDATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
